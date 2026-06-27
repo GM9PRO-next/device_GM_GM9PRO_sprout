227384a7a52ef7cf80b6aa4d71198a32259605ce
yigityanik <yeyanik123@gmail.com>
2026-06-27 08:27:53 +0300

    GM9PRO_sprout: build missing libraries for 14

--- device.mk
+++ device.mk
@@ -198,6 +198,7 @@ PRODUCT_COPY_FILES += \
 # Keystore
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
+    libion.vendor \
     android.hardware.keymaster@3.0.vendor
 
 # Lights
@@ -230,7 +231,8 @@ PRODUCT_COPY_FILES += \
 # Network
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libnetutils.vendor
 
 # OMX
 PRODUCT_PACKAGES += \
@@ -327,7 +329,9 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2 \
     android.hardware.secure_element@1.2.vendor \
-    librmnetctl
+    librmnetctl \
+    libsqlite.vendor \
+    libsysutils.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
