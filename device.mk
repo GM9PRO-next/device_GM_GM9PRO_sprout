e6430c686ea15c3d635085affd564988cbe0efea
yigityanik <yeyanik123@gmail.com>
2026-06-27 07:48:06 +0300

    GM9PRO_sprout: fix missing separator

--- device.mk
+++ device.mk
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
