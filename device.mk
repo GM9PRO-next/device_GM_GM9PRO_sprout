eebc910e78c0d0ad3fe00410259e9e098d84e720
yigityanik <yeyanik123@gmail.com>
2026-06-28 02:47:53 +0300

    GM9PRO_sprout: Switch to common lights HAL

--- device.mk
+++ device.mk
@@ -198,9 +198,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service \
-    android.hardware.light@2.0-impl \
-    lights.sdm660
+    android.hardware.light-service.lineage
 
 # Low Power Whitelist
 PRODUCT_COPY_FILES += \
