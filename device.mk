068c58c0a6b1fcec4e95afde14f9eba31ab718c5
yigityanik <yeyanik123@gmail.com>
2026-06-29 05:52:41 +0300

    GM9PRO_sprout: drop power stats

--- device.mk
+++ device.mk
@@ -296,8 +296,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
