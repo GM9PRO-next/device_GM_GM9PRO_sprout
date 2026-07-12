9bcbfd9d52cb4e5aa01ac5f67cb4a64efd8b840c
yigityanik <yeyanik123@gmail.com>
2026-07-12 16:22:12 +0000

    GM9PRO_sprout: build legacy health stuff
    required for gnss???

--- device.mk
+++ device.mk
@@ -161,7 +161,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
+    android.hardware.health@2.1-impl:64
 
 # HIDL
 PRODUCT_PACKAGES += \
