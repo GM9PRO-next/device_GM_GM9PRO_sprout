e8637fbce1d256b7e50fce52e0566cef7d8b8aa6
yigityanik <yeyanik123@gmail.com>
2026-07-14 22:53:11 +0000

    GM9PRO_sprout: Disable the usage of ConfigStore

--- device.mk
+++ device.mk
@@ -112,6 +112,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
